c977d04181094565bf730673998ae46375912f3e
puneeth-kvsk <puneeth@morphle.in>
2022-11-07 17:25:59 +0530

    Screen functionality commit pre tuning

--- gui_test.py
+++ gui_test.py
@@ -7,29 +7,56 @@ from PyQt5.QtCore import pyqtSlot
 def window():
     app = QApplication(sys.argv)
     widget = QWidget()
-    widget.setGeometry(50, 50, 320, 200)
+    widget.setGeometry(200, 200, 320, 200)
     widget.setWindowTitle("Microtome")
 
     button1 = QPushButton(widget)
     button1.setText("Take Sections")
     button1.move(1, 32)
-    button1.clicked.connect(button1_clicked)
+    button1.clicked.connect(take_sections_button)
 
     button2 = QPushButton(widget)
-    button2.setText("Button2")
+    button2.setText("Teardown Phase 1")
     button2.move(1, 64)
-    button2.clicked.connect(button2_clicked)
+    button2.clicked.connect(teardown_phase_one)
+
+    button2 = QPushButton(widget)
+    button2.setText("Teardown Phase 2")
+    button2.move(1, 96)
+    button2.clicked.connect(teardown_phase_two)
+
+    button2 = QPushButton(widget)
+    button2.setText("Final Process")
+    button2.move(1, 128)
+    button2.clicked.connect(final_process)
+
+    button2 = QPushButton(widget)
+    button2.setText("Discard tissue")
+    button2.move(1, 160)
+    button2.clicked.connect(discard_tissue)
 
     widget.show()
     sys.exit(app.exec_())
 
 
-def button1_clicked():
-    print("Button 1 clicked")
+def take_sections_button():
+    print("Take Section process")
 
 
-def button2_clicked():
-    print("Button 2 clicked")
+def teardown_phase_one():
+    print("Teardown process 1")
+
+
+def teardown_phase_two():
+    print("Teardown process 2")
+
+
+def final_process():
+    print("Final Process")
+
+
+def discard_tissue():
+    print("Discard Tissue")
 
 
 if __name__ == '__main__':
